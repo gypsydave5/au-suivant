f3350e5e86b7471e533d02b588f5474cb6f170c6
David Wickes <dave.wickes@gmail.com>
2020-03-12 23:00:35 +0000

    command line NEXT

---
+++ cmd/suivant.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	suivant "github.com/gypsydave5/au-suivant"
+	"io"
+	"os"
+	"os/exec"
+	"time"
+
+)
+
+func main() {
+	var delay = flag.Int("delay", 10, "delay in seconds")
+	flag.Parse()
+
+	names := readNames(os.Stdin)
+
+	s := suivant.New(names, time.Second* time.Duration(*delay))
+	out := s.Start()
+
+	for name := range out {
+		notifyDriverChange(name)
+	}
+}
+
+func readNames(stdin io.Reader) []string {
+	var names []string
+	scanner := bufio.NewScanner(stdin)
+	for scanner.Scan() {
+		names = append(names, scanner.Text())
+	}
+	return names
+}
+
+func notifyDriverChange(name string) {
+	title := "NEXT!"
+	message := fmt.Sprintf("%s is Next", name)
+	osa, _ := exec.LookPath("osascript")
+	notify := exec.Command(osa, "-e", `display notification "`+message+`" with title "`+title+`"`)
+	speak := exec.Command(osa, "-e", fmt.Sprintf("say %q", message))
+	_ = notify.Run()
+	_ = speak.Run()
+}
---
+++ suivant.go
@@ -0,0 +1,44 @@
+package suivant
+
+import (
+	"time"
+)
+
+type Suivant struct {
+	names []string
+	n int
+	delay time.Duration
+	next  chan string
+	stop  chan interface{}
+}
+
+func New(names []string, delay time.Duration) *Suivant {
+	next, stop := make(chan string), make(chan interface{})
+	return &Suivant{names: names, delay: delay, next: next, stop: stop}
+}
+
+func (s *Suivant) Start() <-chan string {
+	go s.start()
+	return s.next
+}
+
+func (s *Suivant) Stop() {
+	s.stop <- struct{}{}
+}
+
+func (s *Suivant) sendNext() {
+	s.next<- s.names[s.n % len(s.names)]
+	s.n++
+}
+
+func (s *Suivant) start() {
+	s.sendNext()
+	for {
+		select {
+		case <-time.After(s.delay):
+			s.sendNext()
+		case <-s.stop:
+			return
+		}
+	}
+}
